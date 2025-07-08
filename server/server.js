1875ac2b9291f87476de0de06357e32191cecddc
SanthoshKumar211299 <subjansan@gmail.com>
2025-07-08 11:16:10 +0530

    mne

--- server/server.js
+++ server/server.js
@@ -6,34 +6,30 @@ import { Server} from 'socket.io'
 import {connectDB} from './lib/db.js'
 import userRouter from './routes/userRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
-
+import connectCloudinary from './lib/cloudinary.js'
 //create express app and http server
 const app = express();
 const server = http.createServer(app)
-
 //Initialize socket.io server
 export const io = new Server(server,{
   cors:{origin:"*"}
 })
 //store online user
 export const userSocketMap = {}; //{userId: socketId}
-
 //socket.io hanlder
 io.on("connection", (socket)=>{
    const userId = socket.handshake.query.userId;
    console.log("User Connected", userId);
-  
    if (userId) {
     userSocketMap[userId] = socket.id; // Store user properly
      //emit online users to all connected clients
     io.emit("getOnlineUsers",Object.keys(userSocketMap));
    }
-
    socket.on("disconnect", ()=>{
     console.log("User disconnected", userId);
-    if(userId){
+      if (userId) {
       delete userSocketMap[userId];
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
     }
     
    })
@@ -42,16 +38,21 @@ io.on("connection", (socket)=>{
 
 //middleware setup
 app.use(express.json({limit:'4mb'}));
-app.use(cors());
+app.use(cors({
+  origin:"https://chat-app-frontend1-ashy.vercel.app",
+  credentials:true
+
+}));
 
 //route setup
-app.use("/",(req,res)=>res.send("server is live"))
+app.use("/api/status",(req,res)=>res.send("server is live"))
 app.use("/api/auth",userRouter)
 app.use("/api/messages",messageRouter)
 
-
 //connect to mongodb
 await connectDB()
+connectCloudinary()
+
 if(process.env.NODE_ENV !=="production")
 {
 const PORT = process.env.PORT || 5000;
@@ -60,3 +61,4 @@ server.listen(PORT, ()=> console. log("Server is running on PORT:"
 }
 
  export default server
+
