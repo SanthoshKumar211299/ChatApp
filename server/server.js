178a73082ad2233a25de6cbad92ee89937f0f444
SanthoshKumar211299 <subjansan@gmail.com>
2025-07-08 11:33:26 +0530

    mbm

--- server/server.js
+++ server/server.js
@@ -53,12 +53,11 @@ app.use("/api/messages",messageRouter)
 await connectDB()
 connectCloudinary()
 
-if(process.env.NODE_ENV !=="production")
-{
+
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, ()=> console. log("Server is running on PORT:"
 + PORT));
-}
+
 
  export default server
 
